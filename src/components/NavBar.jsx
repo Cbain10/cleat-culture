d097b3f33953329cf269d89efca6746ff35214f6
Christian Bain <christian.bain@imaginelearning.com>
2022-07-26 12:10:41 -1000

    added basic component

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './NavBar.css';
+const NavBar = () => {
+
+    return (
+        <div>
+            <h1 className="navbar">Home</h1>
+            <h1 className="navbar">Projects</h1>
+        </div>
+    )
+};
+
+export default NavBar;
